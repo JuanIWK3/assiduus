e82f5bcd0bebb06bac6c31f0e96fa6d8ac8d2b6d
Juan Israel dos Anjos <86119976+JuanIWK3@users.noreply.github.com>
2023-03-02 00:42:10 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,89 @@
+use std::env;
+
+use dotenv::dotenv;
+use serenity::async_trait;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{CommandResult, StandardFramework};
+use serenity::model::channel::Message;
+use serenity::model::prelude::Ready;
+use serenity::prelude::*;
+
+#[group]
+#[commands(ping)]
+struct General;
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
+}
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    // Set a handler for the `message` event - so that whenever a new message
+    // is received - the closure (or function) passed will be called.
+    //
+    // Event handlers are dispatched through a threadpool, and so multiple
+    // events can be dispatched simultaneously.
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content.starts_with(">") {
+            let lang = msg.content[1..].split_whitespace().next().unwrap();
+            let content = msg.content[2 + lang.len()..].trim();
+
+            msg.delete(ctx.clone()).await.unwrap();
+
+            if let Err(why) = msg
+                .channel_id
+                .say(&ctx.http, format!("```{}\n{}\n```", lang, content))
+                .await
+            {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+
+        if msg.content == "!ping" {
+            let msg = msg.clone();
+            msg.delete(ctx.clone()).await.unwrap();
+
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    // Set a handler to be called on the `ready` event. This is called when a
+    // shard is booted, and a READY payload is sent by Discord. This payload
+    // contains data like the current user's guild Ids, current user data,
+    // private channels, and more.
+    //
+    // In this case, just print what the current user's username is.
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
